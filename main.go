bf4a6ad115a895aee7fbc0bf625db80844683377
Clint <cahenrichs@gmail.com>
2026-07-28 12:09:21 -0500

    started using the slog package

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,7 +15,7 @@ import (
 	"boot.dev/linko/internal/store"
 )
 
-var logger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
+var logger = slog.New(slog.NewTextHandler(os.Stderr, nil))
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -42,7 +42,7 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	}()
 	st, err := store.New(dataDir, logger)
 	if err != nil {
-		logger.Printf("failed to create store: %v\n", err)
+		logger.Info(fmt.Sprintf("failed to create store: %v\n", err))
 		return 1
 	}
 	s := newServer(*st, httpPort, cancel, logger)
@@ -56,11 +56,11 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		logger.Printf("failed to shutdown server: %v\n", err)
+		logger.Info(fmt.Sprintf("failed to shutdown server: %v\n", err))
 		return 1
 	}
 	if serverErr != nil {
-		logger.Printf("server error: %v\n", serverErr)
+		logger.Info(fmt.Sprintf("server error: %v\n", serverErr))
 		return 1
 	}
 	return 0
@@ -68,7 +68,7 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 
 type closeFunc func() error
 
-func initializeLogger(logFile string) (*log.Logger, closeFunc, error) {
+func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
 	if logFile != "" {
 		file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
 		if err != nil {
@@ -85,10 +85,10 @@ func initializeLogger(logFile string) (*log.Logger, closeFunc, error) {
 			}
 			return nil
 		}
-		return log.New(multiWriter, "", log.LstdFlags), close, nil
+		return slog.New(slog.NewTextHandler(multiWriter, nil)), close, nil
 	}
 	close := func() error {
 		return nil
 	}
-	return log.New(os.Stderr, "", log.LstdFlags), close, nil
+	return slog.New(slog.NewTextHandler(os.Stderr, nil)), close, nil
 }
--- server.go
+++ server.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
@@ -16,10 +16,10 @@ type server struct {
 	httpServer *http.Server
 	store      store.Store
 	cancel     context.CancelFunc
-	logger     *log.Logger
+	logger     *slog.Logger
 }
 
-func newServer(store store.Store, port int, cancel context.CancelFunc, logger *log.Logger) *server {
+func newServer(store store.Store, port int, cancel context.CancelFunc, logger *slog.Logger) *server {
 	mux := http.NewServeMux()
 
 	srv := &http.Server{
@@ -54,7 +54,7 @@ func (s *server) start() error {
 	if !ok {
 		return fmt.Errorf("listener address is not a TCP address")
 	}
-	logger.Printf("Linko is running on http://localhost:%d\n", tcpAddr.Port)
+	logger.Info(fmt.Sprintf("Linko is running on http://localhost:%d\n", tcpAddr.Port))
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
@@ -63,7 +63,7 @@ func (s *server) start() error {
 }
 
 func (s *server) shutdown(ctx context.Context) error {
-	logger.Printf("Linko is shutting down\n")
+	logger.Info("Linko is shutting down")
 	return s.httpServer.Shutdown(ctx)
 }
 
